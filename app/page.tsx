22a39fcbf7b311411959a012b3ef6203c951ea54
Akila Dananjaya <akiladananjaya79@gmail.com>
2025-03-16 00:06:14 +0530

    feat: google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 "use client";
 import InvoiceEditor from "../components/invoice-editor";
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMS3Oq79l7dFXG_cqdWdl8YLsf8jh2SHw",
+  authDomain: "freebillgenerator-df99c.firebaseapp.com",
+  projectId: "freebillgenerator-df99c",
+  storageBucket: "freebillgenerator-df99c.firebasestorage.app",
+  messagingSenderId: "238687233245",
+  appId: "1:238687233245:web:4d37e3f4ccb8014f096734",
+  measurementId: "G-CZF00W5T5R",
+};
+
+const app = initializeApp(firebaseConfig);
+isSupported().then((isSupported) => (isSupported ? getAnalytics(app) : null));
 
 export default function Page() {
   return <InvoiceEditor />;
